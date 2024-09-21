7f403956fec4b9b7b359fffa48870f8b44e6d9b6
Ibrahim Sohail <x.boy.barhoomi@gmail.com>
2024-09-20 21:56:27 -0500

    Added the sample code from BEST Robotics website

--- Default_BEST_Program.py
+++ Default_BEST_Program.py
@@ -1,1 +1,120 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyOQfVuhvPxDuQDJra8psif5"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"markdown","source":["Sample code for servos"],"metadata":{"id":"CrIvj3GrINpf"}},{"cell_type":"code","source":["\"\"\"\n","This is the default BEST Robotics program for the Gizmo.\n","This program offers remote control of simple robots using 3 motors and a servo.\n","This may serve as a useful starting point for your team's competition code. You\n","will almost certainly need to edit or extend this code to meet your needs.\n","\n","This code has two control modes: 'Tank Mode' and 'Arcade Mode'. The Start\n","button on your gamepad switches the robot between the two modes.\n","\n","Here are the controls for Tank Mode:\n","Left Joystick Up/Down    - Motor 1 Fwd/Rev\n","Right Joystick Up/Down   - Motor 3 Fwd/Rev\n","\n","Here are the controls for Arcade Mode:\n","Left Joystick Up/Down    - Robot Fwd/Rev\n","Left Joystick Left/Right - Robot Turn Left/Right\n","\n","These controls work in both modes:\n","Right Trigger            - Motor 4 Forward\n","Right Shoulder Button    - Motor 4 Reverse\n","Left Trigger             - Servo 1 to 0 degrees\n","Left Shoulder Button     - Servo 1 to 90 degrees\n","\n","When neither the left trigger nor shoulder button are pressed, the servo will\n","go to 45 degrees.\n","\"\"\"\n","\n","import board\n","import pwmio\n","import digitalio\n","from adafruit_motor import servo\n","from adafruit_simplemath import map_range, constrain\n","from circuitpython_gizmo import Gizmo\n","\n","# the Gizmo object provides access to the data that is held by the field\n","# management system and the gizmo system processor\n","gizmo = Gizmo()\n","\n","pwm_freq = 50 # Hertz\n","min_pulse = 1000 # milliseconds\n","max_pulse = 2000 # milliseconds\n","servo_range = 90  # degrees\n","\n","# Configure the motors & servos for the ports they are connected to\n","motor_left = servo.ContinuousServo(\n","    pwmio.PWMOut(gizmo.MOTOR_1, frequency=pwm_freq),\n","    min_pulse=min_pulse,\n","    max_pulse=max_pulse\n",")\n","motor_right = servo.ContinuousServo(\n","    pwmio.PWMOut(gizmo.MOTOR_3, frequency=pwm_freq),\n","    min_pulse=min_pulse,\n","    max_pulse=max_pulse\n",")\n","motor_task = servo.ContinuousServo(\n","    pwmio.PWMOut(gizmo.MOTOR_4, frequency=pwm_freq),\n","    min_pulse=min_pulse,\n","    max_pulse=max_pulse\n",")\n","servo_task = servo.Servo(\n","    pwmio.PWMOut(gizmo.SERVO_1, frequency=pwm_freq),\n","    actuation_range=servo_range,\n","    min_pulse=min_pulse,\n","    max_pulse=max_pulse\n",")\n","\n","# Configure the built-in LED pin as an output\n","builtin_led = digitalio.DigitalInOut(board.GP25)\n","builtin_led.direction = digitalio.Direction.OUTPUT\n","\n","TANK_MODE = 0\n","ARCADE_MODE = 1\n","\n","mode = TANK_MODE\n","\n","prev_start_button = False\n","\n","# Keep running forever\n","while True:\n","    # Toggle the built-in LED each time through the loop so we can see\n","    # that the program really is running.\n","    builtin_led.value = not builtin_led.value\n","\n","    # Refreshes the information about axis and button states\n","    gizmo.refresh()\n","\n","    # If the start button was pressed, switch control mode\n","    if gizmo.buttons.start and not prev_start_button:\n","        if mode == TANK_MODE:\n","            mode = ARCADE_MODE\n","        elif mode == ARCADE_MODE:\n","            mode = TANK_MODE\n","    prev_start_button = gizmo.buttons.start\n","\n","    if mode == TANK_MODE:\n","        # Convert gamepad axis positions (0 - 255) to motor speeds (-1.0 - 1.0)\n","        motor_left.throttle = map_range(gizmo.axes.left_y, 0, 255, -1.0, 1.0)\n","        motor_right.throttle = map_range(gizmo.axes.right_y, 0, 255, -1.0, 1.0)\n","    elif mode == ARCADE_MODE:\n","        # Mix right joystick axes to control both wheels\n","        speed = map_range(gizmo.axes.left_y, 0, 255, -1.0, 1.0)\n","        steering = map_range(gizmo.axes.left_x, 0, 255, -1.0, 1.0)\n","        motor_left.throttle = constrain(speed - steering, -1.0, 1.0)\n","        motor_right.throttle = constrain(speed + steering, -1.0, 1.0)\n","\n","    # Control task motor with right trigger / shoulder button\n","    if gizmo.buttons.right_trigger:\n","        motor_task.throttle = 1.0\n","    elif gizmo.buttons.right_shoulder:\n","        motor_task.throttle = -1.0\n","    else:\n","        motor_task.throttle = 0.0\n","\n","    # Control task servo with left trigger / shoulder button\n","    if gizmo.buttons.left_trigger:\n","        servo_task.angle = 0\n","    elif gizmo.buttons.left_shoulder:\n","        servo_task.angle = 90\n","    else:\n","        servo_task.angle = 45\n"],"metadata":{"id":"YrTme6OnIbZ3"},"execution_count":null,"outputs":[]}]}
+"""
+This is the default BEST Robotics program for the Gizmo.
+This program offers remote control of simple robots using 3 motors and a servo.
+This may serve as a useful starting point for your team's competition code. You
+will almost certainly need to edit or extend this code to meet your needs.
+
+This code has two control modes: 'Tank Mode' and 'Arcade Mode'. The Start
+button on your gamepad switches the robot between the two modes.
+
+Here are the controls for Tank Mode:
+Left Joystick Up/Down    - Motor 1 Fwd/Rev
+Right Joystick Up/Down   - Motor 3 Fwd/Rev
+
+Here are the controls for Arcade Mode:
+Left Joystick Up/Down    - Robot Fwd/Rev
+Left Joystick Left/Right - Robot Turn Left/Right
+
+These controls work in both modes:
+Right Trigger            - Motor 4 Forward
+Right Shoulder Button    - Motor 4 Reverse
+Left Trigger             - Servo 1 to 0 degrees
+Left Shoulder Button     - Servo 1 to 90 degrees
+
+When neither the left trigger nor shoulder button are pressed, the servo will
+go to 45 degrees.
+"""
+
+import board
+import pwmio
+import digitalio
+from adafruit_motor import servo
+from adafruit_simplemath import map_range, constrain
+from circuitpython_gizmo import Gizmo
+
+# the Gizmo object provides access to the data that is held by the field
+# management system and the gizmo system processor
+gizmo = Gizmo()
+
+pwm_freq = 50 # Hertz
+min_pulse = 1000 # milliseconds
+max_pulse = 2000 # milliseconds
+servo_range = 90  # degrees
+
+# Configure the motors & servos for the ports they are connected to
+motor_left = servo.ContinuousServo(
+    pwmio.PWMOut(gizmo.MOTOR_1, frequency=pwm_freq),
+    min_pulse=min_pulse,
+    max_pulse=max_pulse
+)
+motor_right = servo.ContinuousServo(
+    pwmio.PWMOut(gizmo.MOTOR_3, frequency=pwm_freq),
+    min_pulse=min_pulse,
+    max_pulse=max_pulse
+)
+motor_task = servo.ContinuousServo(
+    pwmio.PWMOut(gizmo.MOTOR_4, frequency=pwm_freq),
+    min_pulse=min_pulse,
+    max_pulse=max_pulse
+)
+servo_task = servo.Servo(
+    pwmio.PWMOut(gizmo.SERVO_1, frequency=pwm_freq),
+    actuation_range=servo_range,
+    min_pulse=min_pulse,
+    max_pulse=max_pulse
+)
+
+# Configure the built-in LED pin as an output
+builtin_led = digitalio.DigitalInOut(board.GP25)
+builtin_led.direction = digitalio.Direction.OUTPUT
+
+TANK_MODE = 0
+ARCADE_MODE = 1
+
+mode = TANK_MODE
+
+prev_start_button = False
+
+# Keep running forever
+while True:
+    # Toggle the built-in LED each time through the loop so we can see
+    # that the program really is running.
+    builtin_led.value = not builtin_led.value
+
+    # Refreshes the information about axis and button states
+    gizmo.refresh()
+
+    # If the start button was pressed, switch control mode
+    if gizmo.buttons.start and not prev_start_button:
+        if mode == TANK_MODE:
+            mode = ARCADE_MODE
+        elif mode == ARCADE_MODE:
+            mode = TANK_MODE
+    prev_start_button = gizmo.buttons.start
+
+    if mode == TANK_MODE:
+        # Convert gamepad axis positions (0 - 255) to motor speeds (-1.0 - 1.0)
+        motor_left.throttle = map_range(gizmo.axes.left_y, 0, 255, -1.0, 1.0)
+        motor_right.throttle = map_range(gizmo.axes.right_y, 0, 255, -1.0, 1.0)
+    elif mode == ARCADE_MODE:
+        # Mix right joystick axes to control both wheels
+        speed = map_range(gizmo.axes.left_y, 0, 255, -1.0, 1.0)
+        steering = map_range(gizmo.axes.left_x, 0, 255, -1.0, 1.0)
+        motor_left.throttle = constrain(speed - steering, -1.0, 1.0)
+        motor_right.throttle = constrain(speed + steering, -1.0, 1.0)
+
+    # Control task motor with right trigger / shoulder button
+    if gizmo.buttons.right_trigger:
+        motor_task.throttle = 1.0
+    elif gizmo.buttons.right_shoulder:
+        motor_task.throttle = -1.0
+    else:
+        motor_task.throttle = 0.0
+
+    # Control task servo with left trigger / shoulder button
+    if gizmo.buttons.left_trigger:
+        servo_task.angle = 0
+    elif gizmo.buttons.left_shoulder:
+        servo_task.angle = 90
+    else:
+        servo_task.angle = 45
